e613c737d4ed46d4af6fb12f2cd45cfa7ca3fdf1
JoEsVilla <j.estra-vill@hotmail.com>
2023-07-27 18:52:10 -0400

    first commit

---
+++ modules.js
@@ -0,0 +1,10 @@
+//CommonJS, every fille is module (by default)
+//Modules - Encapsulated Code (only share minimum)
+
+const names = require('./names')
+const sayHi = require('./utils')
+const data = require('./flavor')
+require('./mind-grenade')
+sayHi('susan')
+sayHi(names.john)
+sayHi(names.peter)
